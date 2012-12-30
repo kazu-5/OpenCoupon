087a7705e6b9e03a4a43332b91a6c21130d24527
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2012-12-30 15:27:13 +0900

    [howto] howto controll

--- app.php
+++ app.php
@@ -3,3 +3,8 @@
 include('NewWorld5.class.php');
 $app = new App();
 $app->mark();
+$app->SetEnv("layout-dir","layout");
+$app->SetEnv("layout","default");
+$app->SetEnv("template-dir","template");
+$app->SetEnv("controller-name","index.php");
+$app->Dispatch();
