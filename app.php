5956f611b0f11eaae323966e64446e038068a68e
kazuhiro yoshimoto <kazuy.dj9@gmail.com>
2013-01-03 21:33:40 +0900

    commit

--- app.php
+++ app.php
@@ -1,14 +1,17 @@
 <?php
 
-include('NewWorld5.class.php');
-$app = new App();
+//include('NewWorld5.class.php');
+//$app = new App();
+include('Coupon.app.php');
+$app = new Coupon();
+
 $app->mark();
 
 //  Set environment.
 $app->SetEnv("controller-name","index.php");
 
 //  layout
-$app->SetEnv("layout-dir","layouts");
+$app->SetEnv("layout-dir","layout");
 $app->SetEnv("layout","default");
 
 //  template
@@ -16,6 +19,13 @@ $app->SetEnv("template-dir","template");
 
 //  config
 $app->config( new CouponConfig() );
+
+//  PDOの取得
+$pdo = $app->pdo();
+$io = $pdo->Connect($database);
+
+//  coupon
+//$app->config( new Coupon() );
 
 //  Do dispatch
 $app->Dispatch();
