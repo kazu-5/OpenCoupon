d8ad13b65cfaf65319772b1d091a390f939531dc
kazuhiro yoshimoto <kazuy.dj9@gmail.com>
2013-01-16 07:48:22 +0900

    buy/indexアクション作成中

--- CouponConfig.class.php
+++ CouponConfig.class.php
@@ -37,6 +37,82 @@ class CouponConfig extends ConfigMgr
 		return $form_config;
 	}
 	
+	function form_buy()
+	{
+		// I create form config.
+		$form_config = new Config;
+	
+		// form name
+		$form_config->name = 'form_buy';
+	
+		// input text
+		$input_name = 'coupon_id';
+		$form_config->input->$input_name->name  = 'coupon_id';
+		$form_config->input->$input_name->type  = 'hidden';
+		$form_config->input->$input_name->value = 'default';
+		
+		// input text
+		$input_name = 'quantity';
+		$form_config->input->$input_name->name  = 'quantity';
+		$form_config->input->$input_name->type  = 'select';
+		$form_config->input->$input_name->id  = 'quantity';
+		$form_config->input->$input_name->style  = 'font-size:1em; height:1.5em;';
+		$form_config->input->$input_name->onchange = 'change_quantity();';
+		
+		for( $i=1; $i<10; $i++){
+			$form_config->input->$input_name->option->value = $i;
+			$form_config->input->$input_name->option->label = $i;
+			/*
+			$option = array();
+			$option['value'] = $i;
+			$option['label'] = $i;
+			$option['style'] = 'text-align:center;';
+			$input['options'][] = $option;
+			*/
+		}
+		
+		//$form_config->input->$input_name->value = 'default';
+		
+		/*
+		//購入内容
+$input = array();
+$input['name'] 	 = 'quantity';
+$input['type']	 = 'select';
+$input['id']	 = 'quantity';
+//$input['style']	 = 'width:40px; text-align:center; font-size:30px; font-weight: 900;';
+$input['style']	 = 'font-size:1em; height:1.5em;';
+$input['onchange'] = 'change_quantity();';
+for( $i=1; $i<10; $i++){
+	$option = array();
+	$option['value'] = $i;
+	$option['label'] = $i;
+	$option['style'] = 'text-align:center;';
+	$input['options'][] = $option;
+}
+$form['input'][]  = $input; 
+		
+		 */
+		// input submit
+		$input_name = 'submit';
+		$form_config->input->$input_name->name  = 'submit';
+		$form_config->input->$input_name->type  = 'submit';
+		$form_config->input->$input_name->value = 'submit';
+		
+		//  フォームを設定する
+		/*
+		$this->form()->AddForm($config);
+		
+		if( $this->form()->Secure($form_name) ){
+			$this->p('Submit form is successful!!');
+		}else{
+			$this->form()->debug($form_name);
+		}
+		*/
+		//	$this->d( Toolbox::toArray($form_config) );
+	
+		return $form_config;
+	}
+	
 	function select_coupon()
 	{
 		$config = $this->select();
--- buy/index.php
+++ buy/index.php
@@ -10,5 +10,19 @@ switch( $action ){
 		$record = $this->pdo()->Quick(" t_coupon.coupon_id = $coupon_id ");
 		//$this->d( $this->pdo()->qus() );
 		$this->d($record);
+		//  Formの設定
+		$coupon_config = new CouponConfig();
+		$config = $coupon_config->form_buy();
+		
+		$this->form()->AddForm($config);
+		
+		/*
+		if( $this->form()->Secure($form_name) ){
+			$this->p('Submit form is successful!!');
+		}else{
+			$this->form()->debug($form_name);
+		}
+		*/
+		include('buy.html');
 		break;
 }
