eb70af9e02bb7a46d88d4bcc109776f8d4a6a91a
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2013-01-22 00:37:48 +0900

    [buy] bug-fix: OnePiece-Framework

--- buy/index.php
+++ buy/index.php
@@ -9,20 +9,24 @@ switch( $action ){
 	case 'index':
 		$record = $this->pdo()->Quick(" t_coupon.coupon_id = $coupon_id ");
 		//$this->d( $this->pdo()->qus() );
-		$this->d($record);
-		//  Formの設定
-		$coupon_config = new CouponConfig();
-		$config = $coupon_config->form_buy();
+		//$this->d($record);
 		
+		//  Formの設定
+		//$coupon_config = new CouponConfig();
+		//$config = $coupon_config->form_buy();
+		//$this->d( Toolbox::toArray($config) );
+		$config = $this->config()->form_buy();
 		$this->form()->AddForm($config);
 		
+		//  Check secure
 		/*
-		if( $this->form()->Secure($form_name) ){
+		if( $this->form()->Secure('form_buy') ){
 			$this->p('Submit form is successful!!');
 		}else{
-			$this->form()->debug($form_name);
+			$this->form()->debug('form_buy');
 		}
 		*/
+		
 		include('buy.html');
 		break;
 }
