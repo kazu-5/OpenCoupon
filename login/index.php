c45cafda93b78cf6080b5ecb4f4ab297b07862b0
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2013-01-25 20:49:37 +0900

    [login] Change: 色々と変更しました。

--- login/index.php
+++ login/index.php
@@ -3,15 +3,29 @@
 
 $this->mark(null,'controller');
 
-if( $id = $this->model('Login')->GetLoginId() ){
-	$this->mark('loggedin','controller');
-}else{
-	$this->mark('Not login','controller');
-	$this->template('form_login.phtml');
-}
-
-
+//  Init form
 $config = $this->config()->form_login();
 $this->form()->AddForm($config);
 
-
+//  Check login
+if( $id = $this->model('Login')->GetLoginId() ){
+	$this->p('既にログインしています。');
+}else{
+	//  Check secure
+	if( $this->form()->Secure('form_login') ){
+		//  Check account
+		$email = $this->form()->GetValue('email',   'form_login');
+		$pass  = $this->form()->GetValue('password','form_login');
+		$email = md5($email);
+		$pass  = md5($pass);
+		
+		$password = $this->pdo()->quick("password <- t_account.email_md5 = $email");
+		if( $pass === $password ){
+			$this->p('ログインしました。');
+			return;
+		}else{
+			$this->p('パスワードが一致しません。');
+		}
+	}
+	$this->template('form_login.phtml');
+}
---
+++ zlib/sql/t_account.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.4
+-- http://www.phpmyadmin.net
+--
+-- ホスト: localhost
+-- 生成時間: 2013 年 1 月 25 日 11:47
+-- サーバのバージョン: 5.1.44
+-- PHP のバージョン: 5.3.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- データベース: `op_coupon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `t_account`
+--
+
+CREATE TABLE IF NOT EXISTS `t_account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email_md5` char(32) NOT NULL COMMENT 'md5(email)',
+  `email` varchar(200) NOT NULL COMMENT 'Blowfish::Encrypt(email)',
+  `password` varchar(32) NOT NULL COMMENT 'md5(password)',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- テーブルのデータをダンプしています `t_account`
+--
+
