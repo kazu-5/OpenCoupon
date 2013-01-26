c3f2a9c5b4908f22b0b28844998beb5e393d43aa
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2013-01-26 16:53:14 +0900

    [Register] 確認画面まで作った。確認がうまくできない

--- CouponConfig.class.php
+++ CouponConfig.class.php
@@ -130,113 +130,131 @@ class CouponConfig extends ConfigMgr
 	function form_register()
 	{
 		$form_config = new Config;
-	
-		// form name
-		$form_config->name = 'form_register';
+		
+		//  form name
+		$form_config->name   = 'form_register';
 
+		//  First name
 		$input_name = 'first_name';
 		$form_config->input->$input_name->label = '姓';
 		$form_config->input->$input_name->required = true;
 		$form_config->input->$input_name->errors->required = '%sが未入力です。';
 		
+		//  Last name
 		$input_name = 'last_name';
 		$form_config->input->$input_name->label = '名';
 		$form_config->input->$input_name->required = true;
 		$form_config->input->$input_name->errors->required = '%sが未入力です。';
 		
+		//  nickname
 		$input_name = 'nick_name';
-		$form_config->input->$input_name->label = 'メールアドレス';
+		$form_config->input->$input_name->label = 'ニックネーム';
 		$form_config->input->$input_name->required = true;
 		$form_config->input->$input_name->errors->required = '%sが未入力です。';
-		
+
+		//  E-mail
 		$input_name = 'email';
 		$form_config->input->$input_name->label = 'メールアドレス';
 		$form_config->input->$input_name->required = true;
 		$form_config->input->$input_name->errors->required = '%sが未入力です。';
-		
+		$form_config->input->$name->validate->permit = 'email';
+
+		//  E-mail (confirm)
 		$input_name = 'email_confirm';
 		$form_config->input->$input_name->label = 'メールアドレス（確認）';
 		$form_config->input->$input_name->required = true;
+		$form_config->input->$input_name->validate->compare = 'email';
 		$form_config->input->$input_name->errors->required = '%sが未入力です。';
-		
+
+		//  Password
 		$input_name = 'password';
 		$form_config->input->$input_name->label = 'パスワード';
 		$form_config->input->$input_name->type  = 'password';
 		$form_config->input->$input_name->required = true;
 		$form_config->input->$input_name->errors->required = '%sが未入力です。';
 
+		//  Password (confirm)
 		$input_name = 'password_confirm';
 		$form_config->input->$input_name->label = 'パスワード（確認）';
 		$form_config->input->$input_name->type  = 'password';
 		$form_config->input->$input_name->required = true;
 		$form_config->input->$input_name->errors->required = '%sが未入力です。';
-		
+
+		//  Gender
 		$input_name = 'gender';
 		$form_config->input->$input_name->label = '性別';
 		$form_config->input->$input_name->type  = 'select';
 		$form_config->input->$input_name->required = true;
 		$form_config->input->$input_name->errors->required = '%sが未入力です。';
-		
-			$form_config->input->$input_name->options->M->label = '男性';
-			$form_config->input->$input_name->options->M->value = 'M';
+			//  Empty			
+			$form_config->input->$input_name->options->e->value = '';
+			//  Male
+			$form_config->input->$input_name->options->m->label = '男性';
+			$form_config->input->$input_name->options->m->value = 'M';
+			//  Female
+			$form_config->input->$input_name->options->f->label = '女性';
+			$form_config->input->$input_name->options->f->value = 'F';
 
-			$form_config->input->$input_name->options->F->label = '女性';
-			$form_config->input->$input_name->options->F->value = 'F';
-		
+		//  Prefe
 		$input_name = 'pref';
 		$form_config->input->$input_name->label = '都道府県';
 		$form_config->input->$input_name->type  = 'select';
 		$form_config->input->$input_name->required = true;
 		$form_config->input->$input_name->errors->required = '%sが未入力です。';
-		
-			$form_config->input->$input_name->options->M->value = '北海道';
-			$form_config->input->$input_name->options->F->value = '東京';
-		
+
+			$form_config->input->$input_name->options->a->value = '';
+			$form_config->input->$input_name->options->b->value = '北海道';
+			$form_config->input->$input_name->options->c->value = '東京都';
+
 		//  birthday
 		$input_name = 'birthday';
 		$form_config->input->$input_name->label  = '生年月日';
 		$form_config->input->$input_name->joint  = '-';
 		$form_config->input->$input_name->cookie = true;
 		$form_config->input->$input_name->validate->permit = 'date';
-		
+			
 			$i = 'year';
 			$form_config->input->$input_name->options->$i->type  = 'select';
 			$form_config->input->$input_name->options->$i->tail  = '-';
 			$form_config->input->$input_name->options->$i->value = '1980';
-
+			
 			for( $n=1; $n<=80; $n++){
 				$v = date('Y') - $n;
 				$form_config->input->$input_name->options->$i->options->$v->value = $v;
 			}
-			
+				
 			$i = 'month';
 			$form_config->input->$input_name->options->$i->type  = 'select';
 			$form_config->input->$input_name->options->$i->tail  = '-';
-			
-			for( $n=1; $n<=12; $n++){
-				$form_config->input->$input_name->options->$i->options->$n->value = $n;
+			$form_config->input->$input_name->options->$i->validate->required  = true;
+				
+			for( $n=0; $n<=12; $n++){
+				$form_config->input->$input_name->options->$i->options->$n->value = $n ? $n: '';
 			}
-			
+				
 			$i = 'day';
 			$form_config->input->$input_name->options->$i->type  = 'select';
-			for( $n=1; $n<=31; $n++){
-				$form_config->input->$input_name->options->$i->options->$n->value = $n;
+			$form_config->input->$input_name->options->$i->validate->required  = true;
+			for( $n=0; $n<=31; $n++){
+				$form_config->input->$input_name->options->$i->options->$n->value = $n ? $n: '';
 			}
 			
+		//  Agree
 		$input_name = 'agree';
 		$form_config->input->$input_name->label = '利用規約';
 		$form_config->input->$input_name->type  = 'checkbox';
-		$form_config->input->$input_name->value = '';
-		
+		$form_config->input->$input_name->validate->required = true;
+			//  Agree option
 			$form_config->input->$input_name->options->yes->label = '利用規約に同意する';
 			$form_config->input->$input_name->options->yes->value = 1;
-		
+			
+		//  submit
 		$input_name = 'submit';
 		$form_config->input->$input_name->type  = 'submit';
 		$form_config->input->$input_name->class  = 'submit';
 		$form_config->input->$input_name->style  = 'font-size: 16px;';
 		$form_config->input->$input_name->value = ' この内容で仮登録する ';
-	
+		
 		return $form_config;
 	}
 	
