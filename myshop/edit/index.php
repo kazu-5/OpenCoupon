06355b88c7001679544b955d7f48716b95dec83d
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2013-02-03 00:47:08 +0900

    [myshop/edit] Complete: All complete

--- myshop/edit/index.php
+++ myshop/edit/index.php
@@ -3,4 +3,46 @@
 
 <?php
 /* @var $this CouponApp */
-$this->mark();
+
+//  My shop ID.
+$shop_id = $this->GetShopID();
+
+//  Form
+$config = $this->config()->form_shop($shop_id);
+$this->form()->AddForm( $config );
+
+//  Action
+$action = $this->GetAction();
+$this->mark($action,'controller');
+
+switch( $action ){
+	case 'index':
+		$this->template('form.phtml');
+		break;
+		
+	case 'confirm':
+		if(!$this->form()->Secure('form_shop') ){
+			$this->template('form.phtml');
+		}else{
+			$this->template('confirm.phtml');
+		}
+		break;
+		
+	case 'commit':
+		if(!$this->form()->Secure('form_shop') ){
+			$this->template('form.phtml');
+		}else{
+			
+			//  Do update
+			$update = $this->config()->update_shop($shop_id);
+			$result = $this->pdo()->update($update);
+			
+			//  View result
+			if( $result !== false ){
+				$this->template('form.phtml',array('message'=>'更新しました'));
+			}else{
+				$this->template('error-update.phtml');
+			}
+		}
+		break;
+}
