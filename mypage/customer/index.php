516ef64bc1aa41c2c5d6ad44b0386c5b7e7edbe7
kazuhiro yoshimoto <kazuy.dj9@gmail.com>
2013-02-19 22:27:19 +0900

    [mypage]作成中

--- mypage/customer/index.php
+++ mypage/customer/index.php
@@ -13,6 +13,20 @@ $id = $this->model('Login')->GetLoginID();
 $config = $this->config()->form_customer( $id );
 $this->form()->AddForm($config);
 
+$config = $this->config()->form_address_change( $id, 1);
+$this->form()->AddForm($config);
+
+//  Button
+$config = $this->config()->button_add_address();
+$this->form()->AddForm($config);
+
+
+//$records = $this->config()->select_address($id);
+//  customer table
+$config = $this->config()->select_address($account_id);
+$t_address = $this->pdo()->select($config);
+$this->d($t_address);
+
 //	Action
 switch( $action ){
 	case 'index':
@@ -51,7 +65,40 @@ switch( $action ){
 			$this->template('form.phtml');
 		}
 		break;
+	
+	case 'address_commit':
+		if( $this->form()->Secure('form_customer') ){
+			//  OK
+			//  Update
+			$update = $this->config()->update_address( $id);
+			$num = $this->pdo()->update($update);
+			$this->template('form.phtml');
+		}else{
+			//  NG
+			$this->template('form.phtml');
+		}
+		break;
 		
+	/*
+	case 'address_input':
+		if( $this->form()->Secure('button_add_address') ){
+			//  OK
+			$update = $this->config()->insert_address( $id );
+		}else{
+			//  NG
+		}
+		break;
+	
+	case 'address_add':
+		if( $this->form()->Secure('button_add_address') ){
+			//  OK
+			$update = $this->config()->insert_address( $id );
+		}else{
+			//  NG
+		}
+		break;
+	*/
+			
 	default:
 		$this->mark("undefined action. ($action)");
 }
