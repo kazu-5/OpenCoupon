b011f6af4ecca5dceb277a4bf42d37a15e253709
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2013-02-24 05:31:00 +0900

    [myshop/photo] New: Added Path2URL method

--- CouponConfig.class.php
+++ CouponConfig.class.php
@@ -677,11 +677,13 @@ class CouponConfig extends ConfigMgr
 		$select = $this->select_photo( $shop_id, 0, $seq_no );
 		$record = $this->pdo()->select($select);
 		
+		//  Set save dir/name. 
+		$config->input->$input_name->save->dir  = "app:/shop/$shop_id";
+		$config->input->$input_name->save->name = '1';
+		
+		//  Set saved file path.
 		if( $record['url'] ){
-			$config->input->$input_name->save->path = $record['url'];
-		}else{
-			$config->input->$input_name->save->dir  = "app:/shop/$shop_id";
-			$config->input->$input_name->save->name = '1';
+			$config->input->$input_name->value = $record['url'];
 		}
 		
 		$input_name = 'submit';
