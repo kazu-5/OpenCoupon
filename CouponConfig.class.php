ea82f38d8c559cfcc6b589e0097619b7a85e3402
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2013-02-24 13:26:31 +0900

    [CouponConfig] Fix: degrade favorite_pref

--- CouponConfig.class.php
+++ CouponConfig.class.php
@@ -235,7 +235,7 @@ class CouponConfig extends ConfigMgr
 			$form_config->input->$input_name->options->f->value = 'F';
 			
 		//  Pref
-		$input_name = 'pref';
+		$input_name = 'favorite_pref';
 		$form_config->input->$input_name->label = '都道府県';
 		$form_config->input->$input_name->type  = 'select';
 		$form_config->input->$input_name->required = true;
