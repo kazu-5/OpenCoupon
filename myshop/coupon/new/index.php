5be5d987a88099c46845e10fbf2473d8ae9bfbb2
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2013-04-11 20:44:18 +0900

    [myshop] Fix: Notice error

--- myshop/coupon/new/index.php
+++ myshop/coupon/new/index.php
@@ -1,15 +1,13 @@
 <?php
-/* 新規クーポン作成ページ */
+/* @var $this CouponApp */
 
 //  My shop ID.
 $shop_id = $this->GetShopID();
 
 //  Form
 $form_config = $this->config()->form_coupon( $shop_id );
-//$this->d($config);
 $this->form()->AddForm( $form_config );
 $form_name = $form_config->name;
-$this->d( Toolbox::toArray($config) );
 
 //  Action
 $action = $this->GetAction();
