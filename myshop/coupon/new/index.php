fa277c1ad3f5a83353d6dc53c59067b120d75589
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2013-05-21 13:36:22 +0900

    [myshop/coupon/new] Change: image uplode & save

--- myshop/coupon/new/index.php
+++ myshop/coupon/new/index.php
@@ -8,6 +8,7 @@ $shop_id = $this->GetShopID();
 $form_config = $this->config()->form_coupon( $shop_id );
 $this->form()->AddForm( $form_config );
 $form_name = $form_config->name;
+//$this->d($form_name);
 //$this->form()->Clear($form_name);
 
 //  Action
@@ -23,7 +24,7 @@ switch( $action ){
 		break;
 
 	case 'confirm':
-		if(!$this->form()->Secure('form_coupon') ){
+		if(!$this->form()->Secure($form_name) ){
 			$data->message  = '入力内容を確かめて下さい。';
 			$data->template = 'form.phtml';
 		}else{
@@ -32,8 +33,7 @@ switch( $action ){
 		break;
 	
 	case 'commit':
-		if( $this->form()->Secure('form_coupon') ){
-				
+		if( $this->form()->Secure($form_name) ){
 			//  Do Insert
 			$config = $this->config()->insert_coupon($shop_id);
 			$coupon_id = $this->pdo()->insert($config);
@@ -42,8 +42,27 @@ switch( $action ){
 			if( $coupon_id === false ){
 				$data->message = 'Couponレコードの作成に失敗しました。';
 			}else{
+				//	Get image path.
+				$path_from = $this->form()->GetInputValue('coupon_image',$form_name);
+				$path_from = $this->ConvertPath('app:/'.$path_from);
+				if( preg_match( '|\.([a-z]{3})$|i', $path_from, $match ) ){
+					$ext = $match[1];
+					$path_to = $this->ConvertPath("app:/shop/$shop_id/$coupon_id/1.$ext");
+				}else{
+					$this->StackError("Does not match extention.");
+				}
+				
+				//	Create directory.
+				mkdir($this->ConvertPath("app:/shop/$shop_id/$coupon_id"));
+				
+				//	Check if file moved.
+				if(!rename( $path_from, $path_to ) ){
+					$this->StackError("File move is failed.");
+				}
+				
+				//	Clear of form.
 				$this->form()->Clear($form_name);
-				$this->Location("app://myshop/coupon/edit/$coupon_id");
+			//	$this->Location("app://myshop/coupon/edit/$coupon_id");
 			}
 		}
 		break;
